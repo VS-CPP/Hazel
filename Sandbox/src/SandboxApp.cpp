a466e5986303544bf9c2568e6c904e68572cdc84
Leonsius <lfirtskhalava@gmail.com>
2020-12-04 00:37:25 +0400

    03 Premake

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -21,5 +21,7 @@ Hazel::Application* Hazel::CreateApplication()
 }
 
 
-/// Main Function Copy and Pasted here from EntryPoint
+/// Main Function Copy and Pasted here from EntryPoint because Hazel.h file include EntryPoint.h file
 // Therefor EntryPiont knows every header files which is accessable for SandboxApp.
+
+/// Main function exist in EntryPoint, That file only launch the SandboxApp, but really Application is SandboxApp
